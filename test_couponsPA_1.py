12d0695edf9359e708999949a1f7e0586668c652
ThrottleNerd <jubacochran@gmail.com>
2024-05-05 22:39:40 +0200

    adding work file

--- test_couponsPA_1.py
+++ test_couponsPA_1.py
@@ -20,7 +20,7 @@ df['Restaurant20To50'].fillna('Unknown', inplace=True)
 
 
 #adding ordinal categories for some of the user attributes. This will help with plots and stat analysis with the whole picture in mind
-
+'''
 category_mapping = {
     '0': 0,
     'less than 1': 1,
@@ -37,7 +37,9 @@ age_category_mapping = {
     '26 to 30': 2,
     '50plus': 3
 }
+'''
 
+'''
 # adding ordinal for age column
 df['AgeOrdinal'] = df['age'].map(age_category_mapping)
 
@@ -63,7 +65,7 @@ df['RestaurantLessThan20_Ordinal'].fillna(-2, inplace=True)
 
 df['Restaurant20To50_Ordinal'] = df['Restaurant20To50'].map(category_mapping)
 df['Restaurant20To50_Ordinal'].fillna(-2, inplace=True)
-
+'''
 #print(df.info())
 
 #Those that said yes
@@ -128,6 +130,9 @@ print(df['temperature'].describe())
 print(df.columns)
 print(df[['coupon']])
 bar_coupons = df[df['coupon']=='Bar']
+
+
+'''
 #print(bar_coupons)
 #print(type(bar_coupons))
 #print(bar_coupons.iloc[3: , :-9])
@@ -154,3 +159,84 @@ acceptance_rate_4_or_more = yes_count_4_or_more / total_responses_4_or_more
 # Print the results
 print(f"Acceptance Rate for 3 or fewer times: {acceptance_rate_3_or_less:.2f}")
 print(f"Acceptance Rate for 4 or more times: {acceptance_rate_4_or_more:.2f}")
+
+#4. Compare the acceptance rate between drivers who go to a bar more than once a month
+#and are over the age of 25 to the all others.  Is there a difference?
+'''
+
+#4. Compare the acceptance rate between drivers who go to a bar more than once a month 
+#and are over the age of 25 to the all others.  Is there a difference?
+
+#Establish a filter for drivers who go to the bar more than once a month
+['never' 'less1' '1~3' 'gt8' 'Unknown' '4~8']
+
+
+
+
+#Drivers who go to the bar more than once a month
+#drivers_bar = bar_coupons.query("Bar == '1~3' or Bar == '4~8' or Bar == 'gt8' ").reset_index()
+
+
+#print(drivers_bar)
+#print(drivers_bar['age'].describe())
+#print(drivers_bar.info())
+#print(drivers_bar['age'].unique())
+
+#Establish a filter for drivers over the age of 25 who go to the bar more than once a month and said they used to coupon
+#age_bar_over25_years_old = drivers_bar.query("age in ['26', '31', '50plus', '41', '36', '46'] and Y == 1").reset_index()
+#print(age_bar_over25_years_old)
+
+#all others filter
+#all_other_ages_going_to_bar = bar_coupons.query("age  == '21' and age == 'below21' ")
+#print(all_other_ages_going_to_bar)
+#print(all_other_ages_going_to_bar.shape[0])
+#print(type(all_other_ages_going_to_bar.shape[0]))
+#Acceptance rates
+
+'''
+total_yes_over25 = age_bar_over25_years_old.shape[0]
+print(total_yes_over25)
+int(total_yes_over25)
+print(type(total_yes_over25))
+total_responses_over25 = all_other_ages_going_to_bar.shape[0]
+
+#Acceptance rate calculation
+
+#acceptance_rate = age_bar_over25_years_old/all_other_ages_going_to_bar
+#print(acceptance_rate)
+'''
+drivers_bar = df[df['coupon'] == 'Bar']
+
+'''
+#Filtering Bar Coupons and assigning to new DF(just playing with different ways of doing this)
+drivers_with_passengars_and_occupations_bar =df[df['coupon'] == 'Bar']
+
+#Filtering for drivers over the age of 25 who accepted the coupon that visit the bar more than once a month. 
+age_bar_over25_years_old = drivers_with_passengars_and_occupations_bar.query("age in ['26', '31', '50plus', '41', '36', '46'] and Bar in ['1~3', '4~8', 'gt8'] and Y == 1")
+
+
+all_others = drivers_bar.query("not (age in ['26', '31', '50plus', '41', '36', '46'] and Bar in ['less1','1~3', '4~8', 'gt8'])")
+#print(all_others)
+
+# Calculating the acceptance rates acceptance = number of yes according to age and frequencey/total responses across all others
+total_yes_over25 = age_bar_over25_years_old['Y'].sum()
+total_responses_over25 = age_bar_over25_years_old.shape[0]
+acceptance_rate_over25 = total_yes_over25 / total_responses_over25 
+
+total_yes_others = all_others['Y'].sum()
+total_responses_others = all_others['Y'].count()
+acceptance_rate_others = total_yes_others / total_responses_others
+
+# Print the results I didn't format the the int to, two decimal places because I wanted to see the raw value since I'm confused about 'never' situation in Bar
+print(f"Acceptance Rate for drivers over 25 who go to the bar frequently and accepted the coupon: {acceptance_rate_over25}")
+print(f"Acceptance Rate for all other bar visitors: {acceptance_rate_others}")
+
+#5. Use the same process to compare the acceptance rate between drivers who go to bars more than once a month
+#and had passengers that were not a kid and had occupations other than farming, fishing, or forestry.
+
+'''
+drivers_with_passengers_and_occupations_bar = df.query("coupon == 'Bar' and passanger != 'Kid(s)' and ~occupation.isin(['farming', 'fishing', 'forestry'])").reset_index()
+print(drivers_with_passengers_and_occupations_bar)
+
+
+
